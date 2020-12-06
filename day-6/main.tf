34e8a664ecf12d41415b4739c0f6f7e1a23b09a6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-06 10:32:15 +0000

    Terraform: Day 6 :golf:

---
+++ day-6/main.tf
@@ -0,0 +1,15 @@
+locals {
+  groups = [for group in split("\n\n", trimspace(file("input.txt"))):
+    [for person in split("\n", group): split("", person)]
+  ]
+  anyone_yes_counts   = [for g in local.groups: length(distinct(flatten(g))) ]
+  everyone_yes_counts = [for g in local.groups: length(setintersection(g...)) ]
+}
+
+output "part_1_answer" {
+  value = sum(local.anyone_yes_counts)
+}
+
+output "part_2_answer" {
+  value = sum(local.everyone_yes_counts)
+}
