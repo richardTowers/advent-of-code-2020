1d21a9a45a94afc7350db65af75eba35957926df
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-10 17:53:11 +0000

    Terraform: Day 9, Part 2
    max_range_length is a tunable parameter - it's a bit of a cheat so we
    don't have to check O(n^2) numbers, which is too slow in terraform. It
    would get there eventually without that setting though.
    
    For my input, the range it finds is 17 items long, so 20 is just big
    enough.

--- day-9/main.tf
+++ day-9/main.tf
@@ -2,6 +2,8 @@ locals {
   input = trimspace(file("input.txt"))
   numbers = [for string in split("\n", local.input): parseint(string, 10)]
   preamble_length = 25
+  max_range_length = 20
+
   invalid_numbers = [for index, number in slice(local.numbers, local.preamble_length, length(local.numbers)):
     number if !contains(flatten([for i, inum in slice(local.numbers, index, index + local.preamble_length):
       [for j, jnum in slice(local.numbers, index, index + local.preamble_length):
@@ -9,8 +11,25 @@ locals {
       ]
     ]), number)
   ]
+  part_1_answer = local.invalid_numbers[0]
+  ranges = flatten([for end_index, _ in local.numbers:
+    [for range_length in range(0, end_index > local.max_range_length ? local.max_range_length : end_index):
+      {
+        end_index: end_index,
+        range_length: range_length,
+        min: min(slice(local.numbers, end_index - range_length, end_index)...),
+        max: max(slice(local.numbers, end_index - range_length, end_index)...),
+      } if range_length > 1 && sum(concat(slice(local.numbers, end_index - range_length, end_index), [0])) == local.part_1_answer
+    ]
+  ])
+  part_2_answer = local.ranges[0]["min"] + local.ranges[0]["max"]
 }
 
-output "answer" {
-  value = local.invalid_numbers[0]
+output "part_1_answer" {
+  value = local.part_1_answer
 }
+
+output "part_2_answer" {
+  value = local.part_2_answer
+}
+
