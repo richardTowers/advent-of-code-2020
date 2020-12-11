2c97dffb5bd1764e7255023bcd553897b7e0979b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-11 08:38:00 +0000

    Count steps in run.sh

--- day-10/run.sh
+++ day-10/run.sh
@@ -2,9 +2,14 @@
 
 set -eu
 
+steps=0
 output_file=$(mktemp -t aoc-terraform-output)
 while ! grep -F 'Apply complete! Resources: 0 added, 0 changed, 0 destroyed.' "$output_file"
 do
   terraform apply -auto-approve | tee "$output_file"
+  steps=$((steps + 1))
+  echo "(Step $steps)"
 done
+
+echo "Completed in $steps steps"
 
