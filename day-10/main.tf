44a28c777fca8f03c621823c98bc44c33b43edd2
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-12-11 08:31:59 +0000

    Terraform: Day 10, Part 2

--- day-10/main.tf
+++ day-10/main.tf
@@ -2,10 +2,11 @@ locals {
   input = trimspace(file("input.txt"))
   numbers = [for string in split("\n", local.input): parseint(string, 10)]
 
+  device = max(local.numbers...) + 3
   adapters = concat(
     [0], # The charging outlet
     local.numbers, # Our adapters
-    [max(local.numbers...) + 3], # The device
+    [local.device]
   )
 
   # Terraform has no numeric sort, so we have to pad the numbers with zeros and sort them alphabetically
@@ -19,9 +20,34 @@ locals {
   difference_counts = {for key, group in local.difference_groups:
     key => length(group)
   }
+
+  # Part 2
+  options = {for adapter in local.sorted_adapters:
+    adapter => [for a in local.sorted_adapters: tostring(a) if a > adapter && a <= adapter + 3]
+  }
+
+  evaluated_options = (fileexists("evaluated_options.json")
+    ? jsondecode(file("evaluated_options.json"))
+    : { "${local.device}": 1 }
+  )
+
+  next_evaluated_options = merge(local.evaluated_options, {for adapter, options in local.options:
+    adapter => try(sum([for key, value in local.evaluated_options: value if contains(options, key)]), null) if
+    length(setintersection(options, keys(local.evaluated_options))) > 0
+  })
+}
+
+resource "local_file" "evaluated_options" {
+  content  = jsonencode(local.next_evaluated_options)
+  file_permission = "0666"
+  filename = "evaluated_options.json"
 }
 
 output "part_1_answer" {
   value = local.difference_counts["1"] * local.difference_counts["3"]
+}
+
+output "part_2_answer" {
+  value = try(local.evaluated_options["0"], "...")
 }
 
